d5c5e22345d106d216aa518f25bb49b2c6ae794e
Michał Nowak <michal.nowak061@gmail.com>
2020-09-26 15:27:36 +0200

    Initial Commit

---
+++ YouTube Clone/Model/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  YouTube Clone
+//
+//  Created by Michał Nowak on 26/09/2020.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = "AIzaSyB9QME2XB3dNG5tTN9tlE9jY8rcUiQ8f0Y"
+    static var API_URL = ""
+}
+
+class Model {
+    func getVideos() {
+        let url = URL(string: Constants.API_URL)
+    }
+}
---
+++ YouTube Clone/Model/Video.swift
@@ -0,0 +1,16 @@
+//
+//  Video.swift
+//  YouTube Clone
+//
+//  Created by Michał Nowak on 26/09/2020.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
